a6ad8b708dbb244ec947a59d581f53c7407f3a69
Indigo Nolan <indigogonolan@gmail.com>
2025-03-15 01:02:42 +0000

    changed front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,36 @@
 import Image from "next/image";
 
+
+export function Button(text: string, link:string, white:boolean) {
+  return (
+      <a
+          className={`rounded-full border border-solid ${white ? 'bg-foreground text-background' : ''} border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center ${white ? "hover:bg-[#494949] dark:hover:bg-[#777777]" : "hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a]"} hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]`}
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+      >
+        {text}
+      </a>
+      )
+}
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-suse)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center ">
+        <b className = "text-7xl">CashCat</b>
+        <ol className="list-inside text-base/6 sm:text-base/6 text-center font-[family-name:var(--font-suse)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            Your money is <b>your</b> money.
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            You deserve peace of mind that it's going to what <b>you</b> want.
           </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          {Button('Deploy now!', '', true)}
+          {Button('Say hi to the cat!', '', false)}
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
