c51b2a5ff42feb9c11243229a639960fec40bcb4
Indigo Nolan <indigogonolan@gmail.com>
2025-05-11 00:48:52 +0100

    Added suspense

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,81 +1,11 @@
-'use client';
-
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { createClient } from '@/app/utils/supabase';
-import { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import {Suspense} from 'react';
+import LoginClient from '../loginClient/page';
+import LoadingScreen from '../components/loading';
 
 export default function Login() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const redirectTo = searchParams.get('redirectTo') || '/budget';
-  const supabase = createClient();
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  useEffect(() => {
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((event) => {
-      if (event === 'SIGNED_IN') {
-        router.push(redirectTo);
-      }
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [router, redirectTo, supabase]);
-
-  if (!isClient) return null;
-
-  return (
-    <div className="min-h-screen bg-background font-[family-name:var(--font-suse)] flex items-center justify-center">
-      <div className="w-full max-w-md p-8 bg-white/[.02] rounded-lg border-b-4">
-        <h1 className="text-2xl font-bold mb-8 text-center">Sign in to CashCat</h1>
-        <Auth 
-          supabaseClient={supabase}
-          appearance={{
-            theme: ThemeSupa,
-            variables: {
-              default: {
-                colors: {
-                  brand: '#bac2ff',
-                  brandAccent: '#bac2ff',
-                  inputBackground: 'rgba(255, 255, 255, 0.05)',
-                  inputText: '#ffffff',
-                  inputBorder: 'rgba(255, 255, 255, 0.15)',
-                }
-              }
-            },
-            style: {
-              button: {
-                border: 'none',
-                borderRadius: '8px',
-                backgroundColor: '#bac2ff',
-                color: '#000000',
-                fontFamily: "SUSE",
-              },
-              anchor: {
-                color: '#bac2ff',
-                fontFamily: "SUSE",
-              },
-              label : {
-                fontFamily: "SUSE"
-              },
-              input: {
-                borderRadius: '8px',
-                fontFamily: "SUSE",
-              },
-            },
-          }}
-          theme="dark"
-          providers={['google']}
-          redirectTo={`${window.location.origin}${redirectTo}`}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <Suspense fallback={<LoadingScreen/>}>
+            <LoginClient/>
+        </Suspense>
+    )
 }
