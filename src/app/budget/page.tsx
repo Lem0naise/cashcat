c027f7feef28805b9026a05aabe72816e591770e
Indigo Nolan <indigogonolan@gmail.com>
2025-05-10 16:39:57 +0100

    changed

--- src/app/budget/page.tsx
+++ src/app/budget/page.tsx
@@ -27,7 +27,7 @@ export default function Home(){
     }, [])
 
     return(
-        <div className=" grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-suse)]">
+        <div className=" grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen max-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-suse)]">
             <main className="fade-in flex flex-col gap-[32px] row-start-2 items-center ">
                 <Logo/>
                 <div className={` grid-cols-2 gap-4 items-center flex-col sm:flex-row`}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
         }, 500);
     };
 
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-suse)]">
       <main className={`${isAnimatingAway ? "fade-out " : ""} transition-all flex flex-col gap-[32px] row-start-2 items-center `}>
