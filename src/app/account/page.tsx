2db0ba24762af6308cdd98d34d389abab8f9393b
Indigo Nolan <indigogonolan@gmail.com>
2025-06-06 22:56:14 +0100

    up: date

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -259,10 +259,11 @@ export default function Account() {
                             <h2 className="text-lg font-semibold mb-4">Update Notes</h2>
                             <div className="flex flex-col gap-4 text-sm text-white/70">
                                 <p className="">
-                                    You are on CashCat <span className="text-green font-medium">0.5.2</span>. The latest features include:
+                                    You are on CashCat <span className="text-green font-medium">0.5.3</span>. The latest features include:
                                 </p>
                                 <ul className="list-disc ml-4">
                                     <li>Support for multiple bank accounts!</li>
+                                    <li>A faster vendor autocomplete</li>
                                     <li>A new notes and reminders section</li>
                                     <li>A 'compare with bank' feature</li>
                                     <li>Routine bug fixes as always</li>
